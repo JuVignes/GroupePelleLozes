8768b911c0e20e4a751526430e6d929e7b52e28a
zeurgue <francois.lozes@ynov.com>
2015-12-15 11:40:34 +0100

    ..

--- AdopUnGeek/models/Professionel.php
+++ AdopUnGeek/models/Professionel.php
@@ -1,18 +1,17 @@
 <?php
 
 
-public static $table = 'ingenieur';
-public static $primary = 'id_ingenieur';
-public static $attributes = [
-    'id_competence',
-    'nom',
-    'prenom',
-];
-
 class Profesionnel extends Model
 {
+    public static $table = 'ingenieur';
+    public static $primary = 'id_ingenieur';
+    public static $attributes = [
+        'id_competence',
+        'nom',
+        'prenom',
+    ];
+    
     function Recherche_competence($competence){
-        global $dbh;
         Model::select('id_comepetence');
     }
 
